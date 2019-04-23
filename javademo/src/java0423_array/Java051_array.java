32a07a6184c709756d0ad1c1e5b20a96d4346eb1
SungMinLEE <ardor1216@gmail.com>
2019-04-23 18:08:24 +0900

    Academy_190423_18:08

---
+++ javademo/src/java0423_array/Java051_array.java
@@ -0,0 +1,27 @@
+package java0423_array;
+
+public class Java051_array {
+
+	public static void main(String[] args) {
+		//가변배열을 생성할 때는 제일 마지막의 배열크기는 지정하지 않는다.
+		/*
+		int[][] num= new int[3][]; //행의 크기는 반드시 지정해야 함
+		num[0] = new int[2]; //0행 2열
+		num[1] = new int[3]; //1행 3열
+		num[2] = new int[4]; //2행 4열
+		*/
+		
+		int[][] num= new int[][] {{1,2},{3,4,5},{6,7,8,9}};
+		
+		for(int row=0; row<num.length; row++) {
+			for(int col=0; col<num[row].length; col++) {
+				System.out.printf("%4d", num[row][col]);
+			}
+			System.out.printf("\n");			
+		}
+		
+
+	}
+
+}
+//가변배열은 2차원 배열부터 가능
---
+++ javademo/src/java0423_array/prob/Prob_02.java
@@ -0,0 +1,66 @@
+package java0423_array.prob;
+/*
+ * 4행 4열 data배열에 가로 세로 합계를 구하는 프로그램을 구현하시오.
+ *  
+ * [출력결과]
+ *  1   2   3   6
+ *  4   5   6  15
+ *  7   8   9  24
+ * 12  15  18  45
+ */
+
+public class Prob_02 {
+
+	public static void main(String[] args) {
+		int[][] data = new int[4][4];
+		int cnt = 1;		
+		
+		// 여기를 구현하시오.
+		int rTotal = 0;
+		int cTotal = 0;
+		
+		for (int row=0; row<data.length-1; row++) {
+			rTotal = 0;
+			for (int col=0; col< data[row].length; col++) {
+				if(col==3) {
+					System.out.printf("%4d", rTotal);
+				} else {	
+					data[row][col] = cnt;
+					rTotal = rTotal + cnt;
+					cnt++;
+					
+					System.out.printf("%4d", data[row][col]);					
+				} 
+			}
+			System.out.printf("\n");
+			
+		}
+		/*
+		data[3][0] = data[0][0] + data[1][0] + data[2][0];
+		data[3][1] = data[0][] + 
+		*/
+		
+		
+		//data[i][j] {
+			
+		}
+		for (int i=0; i< 4; i++) {
+			cTotal = 0;
+			for (int j= 0; j<3; j++) {
+				cTotal = cTotal + data[j][i];				
+			}
+			data[j][i] = cTotal;
+			System.out.printf("%4d", cTotal);
+		}
+		
+		
+	}// end main()
+	/*사고과정
+	 * 4행이나 4열이 아닌 경우, 하나씩 증가
+	 * 4열인 경우, 왼쪽꺼 합계
+	 * 4행인 경우, 위쪽꺼 합계
+	 */
+	
+}// end class
+
+
